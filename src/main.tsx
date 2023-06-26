e322c975dce0b324086b78a556f0b1d0537956e8
Andriy Shymkiv <winamondezik@gmail.com>
2023-06-26 23:31:05 +0300

    chore: add react-query

--- src/main.tsx
+++ src/main.tsx
@@ -9,16 +9,24 @@ import './styles/index.css';
 import { theme } from './theme';
 import { Layout } from './wrappers/Layout';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 const Root = () => (
-  <ThemeProvider theme={theme}>
-    <AuthProvider>
-      <Layout>
-        <AuthGuard>
-          <App />
-        </AuthGuard>
-      </Layout>
-    </AuthProvider>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Layout>
+          <AuthGuard>
+            <App />
+          </AuthGuard>
+        </Layout>
+      </AuthProvider>
+    </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
